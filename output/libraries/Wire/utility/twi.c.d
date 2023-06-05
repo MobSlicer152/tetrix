245c6bda32fb6567fb8f6295d145e4880dba7023
MobSlicer152 <brambleclaw1414@gmail.com>
2023-06-05 13:38:05 -0400

    Remove output

--- output/libraries/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\bramb\source\repos\tetrix\output\libraries\Wire\utility\twi.c.o: \
- C:\Users\bramb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.c \
- C:\Users\bramb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
- C:\Users\bramb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
- C:\Users\bramb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h \
- C:\Users\bramb\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.h
