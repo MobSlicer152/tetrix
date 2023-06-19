100e88737ac6c870688b055d79a3c4c9ae664749
MobSlicer152 <brambleclaw1414@gmail.com>
2023-06-19 11:42:14 -0400

    Robot almost complete

--- tej2o1-01_m-elliot_robot/tej2o1-01_m-elliot_robot.ino
+++ tej2o1-01_m-elliot_robot/tej2o1-01_m-elliot_robot.ino
@@ -3,11 +3,11 @@
 // Name: Elliot McNeil
 // Description: Robot
 
-#include <stdint.h>
-
-#include <Arduino.h>
-
+#ifdef SIMON
 #include <PULSE.h>
+#else
+#include "src/tej2o1-01_m-elliot_utility.h"
+#endif
 
 class Robot
 {
@@ -15,10 +15,17 @@ class Robot
     Robot() = default;
 
     // Initialize the robot
-    Robot(uint8_t leftMotor, uint8_t rightMotor, uint8_t armServo, uint8_t gripperServo, uint8_t lineSensor, uint8_t sonicSensor)
-        : m_leftMotor(leftMotor), m_rightMotor(rightMotor), m_armServo(armServo), m_gripperServo(gripperServo), m_lineSensor(lineSensor), m_sonicSensor(sonicSensor)
+    Robot(uint8_t leftMotor, uint8_t rightMotor, uint8_t armServo,
+          uint8_t gripperServo, uint8_t lineSensor, uint8_t sonicSensor)
+        : m_leftMotor(leftMotor), m_rightMotor(rightMotor),
+          m_armServo(armServo), m_gripperServo(gripperServo),
+          m_lineSensor(lineSensor), m_sonicSensor(sonicSensor)
     {
+#ifdef SIMON
         m_pulse.PulseBegin();
+#else
+        m_pulse = pulse;
+#endif
         SetArm(90);
         SetGripper(0);
         delay(1000);
@@ -26,61 +33,55 @@ class Robot
     }
 
     // Set arm position 0-180 degrees
-    void SetArm(uint32_t degrees)
+    void SetArm(uint16_t degrees)
     {
         Serial.println(String("Arm ") + degrees);
         m_pulse.setServoPosition(m_armServo, degrees);
     }
 
     // Set gripper percentage
-    void SetGripper(uint32_t percent)
+    void SetGripper(uint16_t percent)
     {
-        Serial.println(String("Gripper ") + String(percent) + "%");
+        Serial.println(String("Gripper ") + percent + "%");
         m_pulse.setServoPosition(m_gripperServo, percent / 100.0f * 180);
     }
 
     // Go straight
     void Drive(uint16_t duration, bool backward)
     {
-        Serial.println(String("Drive ") + String(duration) + String(" ") + backward);
+        Serial.println(String("Drive ") + duration + " " + backward);
         m_pulse.setMotorPower(m_rightMotor, 100);
         m_pulse.setMotorPower(m_leftMotor, -100);
-        delay(duration);
-        m_pulse.setMotorPower(m_rightMotor, 0);
-        m_pulse.setMotorPower(m_leftMotor, 0);
-    }
-
-    // Turn left
-    void TurnLeft(uint16_t duration)
-    {
-        Serial.println(String("Left turn ") + duration);
-        m_pulse.setMotorPower(m_rightMotor, 100);
-        m_pulse.setMotorPower(m_leftMotor, -10);
-        delay(duration);
-        m_pulse.setMotorPower(m_rightMotor, 0);
-        m_pulse.setMotorPower(m_leftMotor, 0);
-    }
-
-    // Turn right
-    void TurnRight(uint16_t duration)
-    {
-        Serial.println(String("Left turn ") + duration);
-        m_pulse.setMotorPower(m_rightMotor, 10);
-        m_pulse.setMotorPower(m_leftMotor, -100);
-        delay(duration);
-        m_pulse.setMotorPower(m_rightMotor, 0);
-        m_pulse.setMotorPower(m_leftMotor, 0);
+        if (duration > 0)
+        {
+            delay(duration);
+            Stop();
+        }
     }
 
     // Rotate in place
     void Turn(uint16_t duration, bool left)
     {
-        Serial.println(String("In-place turn ") + String(duration) + String(" left ") + left);
-        m_pulse.setMotorPower(m_rightMotor, left ? 100 : -100);
-        m_pulse.setMotorPower(m_leftMotor, left ? -100 : 100);
-        delay(duration);
-        m_pulse.setMotorPower(m_rightMotor, 0);
-        m_pulse.setMotorPower(m_leftMotor, 0);
+        Serial.println(String("In-place turn ") + duration + " left " + left);
+        m_pulse.setMotorPower(m_rightMotor, left ? 100 : 10);
+        m_pulse.setMotorPower(m_leftMotor, left ? -100 : -10);
+        if (duration > 0)
+        {
+            delay(duration);
+            Stop();
+        }
+    }
+
+    // Detect line
+    bool DetectLine()
+    {
+        return m_pulse.readLineSensor(m_lineSensor) == 0;
+    }
+
+    // Detect object with sonic sensor
+    uint32_t DetectObject()
+    {
+        return m_pulse.readSonicSensorCM(m_sonicSensor);
     }
 
     // Stop motors
@@ -90,9 +91,14 @@ class Robot
         m_pulse.setMotorPower(m_rightMotor, 0);
         m_pulse.setMotorPower(m_leftMotor, 0);
     }
+
   private:
     // PULSE instance
+#ifdef SIMON
     PULSE m_pulse;
+#else
+    PULSE m_pulse;
+#endif
 
     // Pins
     uint8_t m_leftMotor;
@@ -106,14 +112,48 @@ class Robot
 // Set up the robot instance
 Robot robot;
 
-PULSE pulse;
-
 void setup()
 {
-    robot = Robot(1, 2, 2, 1, 2, 3);
-    robot.Drive(1000, false);
+    robot = Robot(1, 2, 3, 1, 2, 3);
+    Util::Initialize();
+}
+
+void AvoidObstacles()
+{
+    robot.Turn(2500, false);
+    robot.Drive(2500, false);
+    robot.Turn(2500, true);
+}
+
+void End()
+{
+    robot.Stop();
+    robot.SetArm(45);
+    robot.SetGripper(100);
+
+    // End
+    while (true)
+    {
+#ifndef SIMON
+        Morse::Output("Сделаный!+");
+#endif
+    }
 }
 
 void loop()
 {
+
+    if (robot.DetectObject() <= 20)
+    {
+        AvoidObstacles();
+    }
+    else
+    {
+        robot.Drive(0, false);
+    }
+
+    if (robot.DetectLine())
+    {
+        End();
+    }
 }
